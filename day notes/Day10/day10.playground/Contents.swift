e20bd248396a58d32acac3032dfc67ac76737144
Ivan Lara <ivan.lara90@yahoo.com>
2026-03-23 20:39:15 -0700

    Day 10 Section 2: computed properties + short test

--- day notes/Day10/day10.playground/Contents.swift
+++ day notes/Day10/day10.playground/Contents.swift
@@ -4,7 +4,7 @@ import Cocoa
 
 ///1, how to create your own structs
 
-struct Album {
+/* struct Album {
     let title: String
     let artist: String
     let year: Int
@@ -37,12 +37,14 @@ struct Employee {
         }
     }
 }
+ 
+
 /// instance of struct
-var archer = Employee(name: "Sterling Arhcer", vacationRemaining: 14)
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
                       ///initializer of struct ^^^
 archer.takeVacation(days: 5)
 print(archer.vacationRemaining)
-
+ 
 ///What’s the difference between a struct and a tuple?
 ///a tuple is effectively just a struct without a name, like an anonymous struct
 ///use tuples when you want to return two or more arbitrary pieces of values from a function, but prefer structs when you have some fixed data you want to send or receive multiple times.
@@ -98,4 +100,56 @@ print(archer.vacationRemaining)
 ///A method that is not marked as mutating cannot call a mutating function – you must mark them both as mutating
 
 
+///How to compute property values dynamically
+///Structs can have two kinds of property: a stored property is a variable or constant that holds a piece of data inside an instance of the struct, and a computed property calculates the value of the property dynamically every time it’s accessed
+
+*/
+  
+struct Employee {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int {
+        get {
+        vacationAllocated - vacationTaken
+        }
+        
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+archer.vacationRemaining = 5
+print(archer.vacationAllocated)
+
+/// When should you use a computed property or a stored property?
+
+/// Properties let us attach information to structs, and Swift gives us two variations: stored properties, where a value is stashed away in some memory to be used later, and computed properties, where a value is recomputed every time it’s called. Behind the scenes, a computed property is effectively just a function call that happens to belong to your struct.
+
+///computed property example
+struct Wine {
+    var age: Int
+    var isVintage: Bool
+    var price: Int {
+        if isVintage {
+            return age + 20
+        } else {
+            return age + 5
+        }
+    }
+}
+let malbec = Wine(age: 2, isVintage: true)
+print(malbec.price)
+
+struct Swordfighter {
+    var name: String
+    var introduction: String {
+        return "Hello, my name is \(name)."
+    }
+}
+let inigo = Swordfighter(name: "Inigo Montoya")
 
