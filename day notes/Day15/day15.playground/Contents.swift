8a9d44febb13d5febbd3af14d4e95c5e71dd4377
Ivan Lara <ivan.lara90@yahoo.com>
2026-04-10 21:53:16 -0700

    Started day 15 - Review of days 1-14

---
+++ day notes/Day15/day15.playground/Contents.swift
@@ -0,0 +1,75 @@
+import Cocoa
+
+///** CONSTANTS AND VARIABLES
+
+var name = "ted"
+name = "rebecca"
+
+let user = "DAPHNE"
+print(user)
+
+
+/// * *STRINGS
+
+let actor = "Tom Cruise"
+
+let quote = "He tapped a sign saying \"believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+apple engineer
+"""
+
+print(actor.count)
+
+print(quote.hasPrefix("He"))
+print(quote.hasSuffix("Away."))
+
+/// ** INTS
+
+let score = 10
+let higherScore = score + 10
+let halvedSCore = score / 2
+
+var count = 10
+counter += 5
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let id = Int.random(in: 1...1000)
+
+/// ** DOUBLES
+
+let score = 3.1
+
+/// ** BOOL
+
+let goodDogs = true
+let gameOver = false
+var isSaved = false
+isSaved.toggle()
+
+/// **STRING INTERPOLATION
+
+let name = "taylor"
+let age = 26
+let message = "I'm \(name) and im \(age) years old"
+print(message)
+
+/// ** ARRAYS
+
+var colors = ["Red", "Blue", "Yellow"]
+let numbers = [4, 8, 15, 26]
+var readings = [0.1, 0.4, 0.5]
+
+print(colors[0])
+print(readings[2]
+
+colors.append("Tartan")
+colors.remove(at: 0)
+print(colors.count)
+    
+print(colors.contains("Octarine"))
+ 
---
+++ day notes/Day15/notes.md
@@ -0,0 +1,5 @@
+# Day 15 Notes - Swift review Days 1-15 WIP
+
+## Day 15 - review
+
+
