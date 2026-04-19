90f4269e407530159d1b9cd6f0c62d5de9a99413
Ivan Lara <ivan.lara90@yahoo.com>
2026-04-18 20:13:14 -0700

    Day20: Stacks (VStack, HStack, ZStack) notes and GuessTheFlag project started

---
+++ day notes/Day20/day20.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+/*
+ 
+**DAY 20: Project 2: GUESS THE FLAG**
+
+- USING STACKS TO ARRANGE VIEWS
+- there are 3 manin stacks in SwiftUI
+ + VStack (vertical)
+ + HStack (horizontal)
+ + ZStack (depth/overlap)
+ 
+- VStack and HStack both support spacing and alignment parameters
+- Spacer() to push content to one side - multiple spacers divide space equally between them
+- ZStack draws views back to front (no spacing, but supports alignment)
+- Swift UI can infer a VStack without explicitly writing one, but being explicit gives you more control
+
---
+++ day notes/Day20/notes.md
@@ -0,0 +1,7 @@
+# Day 20 Notes - Project 2: Guess the Flag, Part 1 WIP
+
+- In this second SwiftUI project we’re going to be building a guessing game that helps users learn some of the many flags of the world.
+- introduce you to whole range of new SwiftUI functionality: stacks, buttons, images, alerts, asset catalogs, and more.
+
+- Learned about HStack, VStack, and ZStack
+- created a 3 x 3 grid having 3 Vstacks nested inside an HStack
