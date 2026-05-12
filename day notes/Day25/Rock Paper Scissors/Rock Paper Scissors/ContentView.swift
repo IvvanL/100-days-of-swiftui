80f04b8c904851a7e2486fbd502d106d31efaa25
Ivan Lara <ivan.lara90@yahoo.com>
2026-05-11 21:05:03 -0700

    Updated day 25 challenge project code

--- day notes/Day25/Rock Paper Scissors/Rock Paper Scissors/ContentView.swift
+++ day notes/Day25/Rock Paper Scissors/Rock Paper Scissors/ContentView.swift
@@ -9,21 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var Rock = ["Rock", "Paper", "Scissors"]
-    @State private var ChoiceWinner = "Winner"
-    @State private var ChoiceLoser = "Loser "
+    @State private var moves = ["Rock", "Paper", "Scissors"]
+    @State private var outcome = false
+    @State private var currentScore = 0
+    @State private var questionsAsked = 0
+    @State private var randomMove = Int.random(in: 0..<3)
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Text("Rock Paper Scissors")
+                    .font(.largeTitle)
+            }
+            
+            HStack {
+                    Text("Computer: \(moves[randomMove])")
+                    Text("Player 1:") {
+                        Button(moves
+                    }
+                }
+            VStack {
+                Text("Your Score: \(currentScore)")
+                Text("Round: \(questionsAsked)")
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
