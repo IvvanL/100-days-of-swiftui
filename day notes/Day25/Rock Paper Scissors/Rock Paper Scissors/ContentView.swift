fb20330715b1d8dfe8abf9d72a94ad16897cf5a6
Ivan Lara <ivan.lara90@yahoo.com>
2026-05-14 21:32:40 -0700

    Updated Day 25 challenge project code

--- day notes/Day25/Rock Paper Scissors/Rock Paper Scissors/ContentView.swift
+++ day notes/Day25/Rock Paper Scissors/Rock Paper Scissors/ContentView.swift
@@ -9,54 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let winningMoves = ["Paper", "Scissors", "Rock"]
     let moves = ["Rock", "Paper", "Scissors"]
     @State private var outcome = false
     @State private var currentScore = 0
     @State private var questionsAsked = 0
     @State private var randomMove = Int.random(in: 0..<3)
+    @State private var playerHasChosen = false
     
     var body: some View {
         VStack {
-            VStack {
-                Text("Rock Paper Scissors")
-                    .font(.largeTitle)
-            }
+            Text("Rock Paper Scissors")
+                .font(.largeTitle)
             
             HStack {
-                    Text("Computer: \(moves[randomMove])")
-                        Button("Rock") {
-                            // action goes here
-                        }
-                        .padding()
-                        .frame(width: 100, height: 50)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
                 
-                        Button("Paper") {
-                        }
-                        .padding()
-                        .frame(width: 100, height: 50)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-            
-                        Button("Scissors") {
-                        }
-                        .padding()
-                        .frame(width: 100, height: 50)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                Button("Rock") {
+                    playerHasChosen = true {
+                        if moves[randomMove] == moves[0] || moves[1] {
+                            currentScore += 1
+                        } else {
+                            currentScore -= 1
                         }
                     }
                 }
+                .padding()
+                .frame(width: 100, height: 50)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+                
+                Button("Paper") {
+                    playerHasChosen = true
+                }
+                .padding()
+                .frame(width: 100, height: 50)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+                
+                Button("Scissors") {
+                    playerHasChosen = true
+                }
+                .padding()
+                .frame(width: 100, height: 50)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+            }
+            if playerHasChosen {
+                Text("Computer: \(moves[randomMove])")
+            }
+            
             VStack {
+                Text("You \(outcome ? "Win" : "Lose")")
                 Text("Your Score: \(currentScore)")
                 Text("Round: \(questionsAsked)")
             }
         }
-
+    }
+}
 #Preview {
     ContentView()
 }
