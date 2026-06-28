ba14426cb53da605bfd285a0b07b3060d2ecccd6
Ivan Lara <ivan.lara90@yahoo.com>
2026-06-27 19:48:40 -0700

    Day36 section 1 completed

---
+++ day notes/Day36/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ivan Lara on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
